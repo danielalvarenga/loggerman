a529745a5928ffdc2b0ff04c983be93982b03980
Daniel Alvarenga <danielalvarengalima@gmail.com>
2016-09-19 16:29:11 -0300

    add tests and refactore format setter

--- lib/loggerman.rb
+++ lib/loggerman.rb
@@ -8,12 +8,12 @@ require 'loggerman/formatters/key_value_formatter'
 
 module Loggerman
   module_function
-	mattr_accessor :logger, :application, :formatters, :format, :log_files
+	mattr_accessor :logger, :application, :formatters, :formatter, :log_files
 
   def setup(app)
   	self.application = app
   	set_formatters
-  	set_format
+  	set_formatter
   	set_log_files
   	set_logger
   end
@@ -22,8 +22,8 @@ module Loggerman
   	Loggerman.formatters = {json: Loggerman::Formatters::JsonFormatter.new, key_value: Loggerman::Formatters::KeyValueFormatter.new}
   end
 
-  def set_format
-  	Loggerman.format = loggerman_config.format || :key_value
+  def set_formatter
+  	Loggerman.formatter = Loggerman.formatters.fetch( loggerman_config.format || :key_value )
   end
 
   def set_log_files
@@ -31,7 +31,7 @@ module Loggerman
   end
 
   def set_logger
-  	Loggerman.logger = Loggerman::Logger.new("#{Rails.root}/log/#{Rails.env.to_s}.log")
+  	Loggerman.logger = Loggerman::Logger.new(defined?(Rails) ? "#{Rails.root}/log/#{Rails.env.to_s}.log" : "rails_default.log")
   end
 
   def loggerman_config
--- spec/loggerman_spec.rb
+++ spec/loggerman_spec.rb
@@ -5,7 +5,42 @@ describe Loggerman do
     expect(Loggerman::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'setup' do
+
+    let(:app_config) do
+      double(config:
+              ActiveSupport::OrderedOptions.new.tap do |config|
+                config.loggerman = ActiveSupport::OrderedOptions.new
+                config.loggerman.log_files = { error: 'my_error_logs.log' }
+              end
+            )
+    end
+
+    it "not set format" do
+      Loggerman.setup(app_config)
+      expect(Loggerman.formatter).to be_is_a Loggerman::Formatters::KeyValueFormatter
+    end
+
+    it "set json format" do
+    	app_config.config.loggerman.format = :json
+      Loggerman.setup(app_config)
+      expect(Loggerman.formatter).to be_is_a Loggerman::Formatters::JsonFormatter
+    end
+
+    it "set key_value format" do
+    	app_config.config.loggerman.format = :key_value
+      Loggerman.setup(app_config)
+      expect(Loggerman.formatter).to be_is_a Loggerman::Formatters::KeyValueFormatter
+    end
+
+    it "custom error log file" do
+      Loggerman.setup(app_config)
+      expect(Loggerman.log_files).to eq({ error: 'my_error_logs.log' })
+    end
+
+    it "set logger" do
+      Loggerman.setup(app_config)
+      expect(Loggerman.logger).to be_is_a Loggerman::Logger
+    end
   end
 end
